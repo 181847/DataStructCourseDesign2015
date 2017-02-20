46d498f22298b4ef03eb7f7a38334b474655559a
181847 <753090084@qq.com>
2017-02-20 22:06:13 +0800

    用于InfoSet中的过滤器
    InfoSet中的一些对于Info的操作可以通过这个对象来将某些特定信息的Info类型过滤掉。

---
+++ src/infoInterface/IInfoFilter.java
@@ -0,0 +1,12 @@
+package infoInterface;
+import info.Info;
+
+public interface IInfoFilter {
+	/**
+	 * 针对当前Info中的信息返回真假。
+	 * @param info 信息体。
+	 * @return 满足Filter中的要求返回真，
+	 * 不满足返回假。
+	 */
+	public boolean check(Info info);
+}
