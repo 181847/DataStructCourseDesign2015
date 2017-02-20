33b89d0182aed212ecc54bb11b21a9b3dc548736
181847 <753090084@qq.com>
2017-02-20 22:10:13 +0800

    IInfoGetter专门用来获取一个Info中的container中的特殊信息
    比如一个Info的container存了一个Student类型，某个IInfoGetter能够通过这个Info获得Student的名字，在这个InfoGetter内部需要进行Object到Student的强制类型转换。

---
+++ src/infoInterface/IInfoGetter.java
@@ -0,0 +1,11 @@
+package infoInterface;
+import info.Info;
+
+public interface IInfoGetter {
+	/**
+	 * 获取Info对象当中指定的信息。
+	 * @param info 信息体。
+	 * @return 信息体中的字符串信息。
+	 */
+	public String pickInfo(Info info);
+}
