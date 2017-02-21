fe997d21d35971568bfc90b3d2318bd404462391
181847 <753090084@qq.com>
2017-02-21 22:44:23 +0800

    创建一个DoubleLinkedInfo类型，父类是InfoWithContainer
    这个类用来代替原来的Info类型，原来的Info类型是作为一个双向链表的节点的，创建这个类来更准确的定义这个双向链表节点

---
+++ src/info/DoubleLinkedInfo.java
@@ -0,0 +1,63 @@
+package info;
+
+import basicInterface.IInfo;
+
+public class DoubleLinkedInfo extends InfoWithContainer implements IInfo {
+	/**
+	 * 前一个信息体。
+	 */
+	public DoubleLinkedInfo prec;
+	
+	/**
+	 * 下一个信息体。
+	 */
+	public DoubleLinkedInfo next;
+	
+	/**
+	 * 创建一个双重连接的节点Info对象。
+	 * @param container 
+	 * 				这个Info对象当中的具体信息。
+	 */
+	public DoubleLinkedInfo(Object container){
+		super(container);
+	}
+	
+	/**
+	 * 默认的构造方法创建一个container为null的双重链接的节点Info。
+	 */
+	public DoubleLinkedInfo(){
+		super(null);
+	}
+	
+	/**
+	 * 获取前一个Info信息体。
+	 * @return 前一个Info信息体。
+	 */
+	public InfoWithContainer getPrec() {
+		return prec;
+	}
+	
+	/**
+	 * 设置前一个Info信息体。
+	 * @param prec 要设置的前一个信息体。
+	 */
+	public void setPrec(DoubleLinkedInfo prec) {
+		this.prec = prec;
+	}
+	
+	/**
+	 * 获取下一个信息体。
+	 * @return 下一个信息体。
+	 */
+	public IInfo getNext() {
+		return next;
+	}
+	
+	/**
+	 * 设置下一个信息体。
+	 * @param next 下一个信息体。
+	 */
+	public void setNext(DoubleLinkedInfo next) {
+		this.next = next;
+	}
+}
