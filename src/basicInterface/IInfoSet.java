7356876830ce128a5fcd553fc905eb7695fc50c6
181847 <753090084@qq.com>
2017-02-21 22:38:59 +0800

    创建一个IInfoSet接口，这个接口定义了三个与信息体集合相关的方法
    搜索接口，通过字符串，特定的信息提取器，过滤器，从固定的信息体中获取相关的信息体集合，以一个信息体返回。
    遍历信息体，通过遍历者和过滤器，对信息体集合内部的指定信息体调用遍历者的方法。
    以IInfo数组的形式将所有信息体的信息返回出来。

---
+++ src/basicInterface/IInfoSet.java
@@ -0,0 +1,11 @@
+package basicInterface;
+
+import infoInterface.IInfoFilter;
+import infoInterface.IInfoGetter;
+import infoInterface.IInfoTraverser;
+
+public interface IInfoSet {
+	public IInfoSet search(String searchInfo, IInfoGetter getter, IInfoFilter filter);
+	public int traverseInfo(IInfoTraverser traverser, IInfoFilter filter);
+	public IInfo[] getInfos();
+}
