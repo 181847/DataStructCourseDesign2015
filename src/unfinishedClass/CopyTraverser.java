a30717fef3d0358acc1cff6f1ffe71a8b792a9e6
181847 <753090084@qq.com>
2017-02-21 22:53:26 +0800

    创建一个copyTraverser，用来遍历信息体的时候同时复制一份。
    这个Traverser内部有一个DLLInfoSet成员变量，当它每遍历到一个Info，就会复制一个这个Info类型到自己的成员变量中，当遍历完成之后，程序猿就获得了关于某个信息体内部的所有信息。

---
+++ src/unfinishedClass/CopyTraverser.java
@@ -0,0 +1,43 @@
+package unfinishedClass;
+
+import basicInterface.IInfo;
+import basicTool.MyLogger;
+import infoInterface.IInfoTraverser;
+
+/**
+ * 这个遍历装置能够将遍历到的Info加入内部存储的DoubleLoopLinkedInfoSet中。
+ * @author 75309
+ *
+ */
+public class CopyTraverser implements IInfoTraverser {
+	DoubleLoopLinkedInfoSet containerSet;
+
+	public CopyTraverser(DoubleLoopLinkedInfoSet containerSet){
+		this.containerSet = containerSet;
+	}
+	
+	public DoubleLoopLinkedInfoSet getcontainerSet(){
+		return containerSet;
+	}
+	
+	public void setcontainerSet(DoubleLoopLinkedInfoSet containerSet){
+		this.containerSet = containerSet;
+	}
+	
+	@Override
+	public int traverserInfo(IInfo info) {
+		if (info == null){
+			MyLogger.logError("CopyTraverser准备复制一个info对象，"
+					+ "但是参数info为null，无法复制。");
+			return 0;
+		}
+		if (info.getContainer() == null){
+			MyLogger.logError("CopyTraverser准备复制一个info对象，"
+					+ "但是info内部的container为null，没有信息，复制失败。");
+			return 0;
+		}
+		containerSet.insertInfo(info);
+		return 1;
+	}
+
+}
