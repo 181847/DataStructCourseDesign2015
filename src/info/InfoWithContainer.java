8f5a923471e0fbd256b602b78ab729bf6cb26fcc
181847 <753090084@qq.com>
2017-02-21 22:42:22 +0800

    创建一个InfoWithContainer实现IInfo接口的方法

---
+++ src/info/InfoWithContainer.java
@@ -0,0 +1,46 @@
+package info;
+
+import basicInterface.IInfo;
+
+public class InfoWithContainer implements IInfo{
+	/**
+	 * 存储信息的对象，
+	 * 之所以创建这么一个对象是为了使得多个Info对象能够操作同一个信息
+	 */
+	public Object container;
+	
+	/**
+	 * @param container 信息的承载体，可以是任何对象。
+	 */
+	public InfoWithContainer(Object container){
+		this.container = container;
+	}
+	
+	/**
+	 * 默认构造方法，
+	 * 内部信息全部为null。
+	 */
+	public InfoWithContainer(){
+		//Empty Body
+	}
+	
+	/**
+	 * 获取当前Info对象中的信息承载体。
+	 * @return 包含信息的Object对象。
+	 */
+	@Override
+	public Object getContainer() {
+		return container;
+	}
+
+	/**
+	 * 设置当前Info对象中的信息承载体。
+	 * @param container 包含信息的Object对象。
+	 */
+	@Override
+	public void setContainer(Object container) {
+		this.container = container;
+	}
+
+	
+}
