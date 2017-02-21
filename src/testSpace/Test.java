00a8e4143098062d507755c2c0c027c8687fed4c
181847 <753090084@qq.com>
2017-02-21 23:19:04 +0800

    增加一个测试环境类型
    这个测试环境内部有一个静态成员Student数组，通过prepare()方法来为这个Student数组创建Student实例，方便测试子类调用测试用例。

---
+++ src/testSpace/Test.java
@@ -0,0 +1,13 @@
+package testSpace;
+
+import student.Student;
+
+public class Test {
+	public static Student[] stus;
+	
+	public static void prepare(){
+		stus = new Student[]{new Student("abc", "1"), 
+				new Student("acd", "2"), 
+				new Student("abe", "3")};
+	}
+}
