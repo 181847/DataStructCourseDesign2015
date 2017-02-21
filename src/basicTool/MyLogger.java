575527dd8adffc8beffa4181fa5ec04629b99c9d
181847 <753090084@qq.com>
2017-02-21 23:14:11 +0800

    日志记录对象增加一个特定的记录错误信息的方法，自动在信息前面加上“Log ERROR!”。

--- src/basicTool/MyLogger.java
+++ src/basicTool/MyLogger.java
@@ -6,14 +6,22 @@ public class MyLogger {
 	 * @param message 信息。
 	 */
 	public static void log(String message){
-		System.out.println("Logger Message: " + message);
+		System.out.println("Log Message: " + message);
+	}
+	
+	/**
+	 * 记录错误信息。
+	 * @param errorMessage 要记录的错误信息。
+	 */
+	public static void logError(String errorMessage){
+		System.out.println("Log ERROR! " + errorMessage);
 	}
 	
 	/**
 	 * 记录异常对象，向控制台打印异常对象的StackTrace。
 	 * @param e 异常对象。
 	 */
-	public static void log(Exception e){
+	public static void logException(Exception e){
 		System.out.println("Logger Message: ");
 		e.printStackTrace();
 	}
