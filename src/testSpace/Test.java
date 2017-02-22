129bcaac0a962a24db4bfff10e6b59b71f655e5a
181847 <753090084@qq.com>
2017-02-22 12:47:26 +0800

    无关紧要的代码格式修改

--- src/testSpace/Test.java
+++ src/testSpace/Test.java
@@ -6,7 +6,8 @@ public class Test {
 	public static Student[] stus;
 	
 	public static void prepare(){
-		stus = new Student[]{new Student("abc", "1"), 
+		stus = new Student[]{
+				new Student("abc", "1"), 
 				new Student("acd", "2"), 
 				new Student("abe", "3")};
 	}
