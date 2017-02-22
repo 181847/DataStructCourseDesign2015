124ac84ed6f214d3b076300a5e8812158a50aea1
181847 <753090084@qq.com>
2017-02-23 00:01:37 +0800

    测试环境改为新的Student类，并且增加测试对象

--- src/testSpace/Test.java
+++ src/testSpace/Test.java
@@ -1,14 +1,22 @@
 package testSpace;
 
-import student.Student;
+import unfinishedClass.Student;
 
 public class Test {
 	public static Student[] stus;
 	
 	public static void prepare(){
 		stus = new Student[]{
-				new Student("abc", "1"), 
-				new Student("abe", "3"), 
-				new Student("acd", "2")};
+				new Student("2002015", "张光和", 1, "大一", "物流工程"), 
+				new Student("2002002", "张植强", 1, "大一", "动力工程"), 
+				new Student("2002003", "槐心", 1, "大二", "智能理论"), 
+				new Student("2002004", "王小勇", 1, "大一", "材料科学"), 
+				new Student("2002005", "王历历", 1, "大三", "文化与法制"), 
+				new Student("2002006", "吴孟达", 1, "大三", "计算机"), 
+				new Student("2002007", "金嫣红", 2, "大二", "计算机"), 
+				new Student("2002008", "刘长艳", 2, "大一", "数学理论"), 
+				new Student("2002009", "许强", 1, "大三", "机械理论"), 
+				new Student("20020010", "杨小鹃", 2, "大一", "文化与法制"), 
+				new Student("20020011", "岳不群", 1, "大一", "体育")};
 	}
 }
