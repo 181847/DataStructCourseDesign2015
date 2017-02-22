2742f8dec89bcab20ea00cbb96f4a51987512730
181847 <753090084@qq.com>
2017-02-22 23:45:17 +0800

    先前有一个RegistObject类实现了有关名字的接口，父类是IndexHolder，再以RegistObject类为父类，创建一个实现了IPinyinHolder接口的类
    IPinyinHolder是一个在原来IPinyinNameHolder接口上修改而来的，这个接口增加了两个关于获取和设置短拼音名字的方法，短拼音就是把汉字转换为拼音的时候只提取首字母。
    
    有关RegistObject/IPinyinHolder的详细被错误的包含到了之前的一些Commit里面去了，详情查看（2ae14253b0c25bba3009795f28f20634409504be）号Commit“移动已完成的类”

---
+++ src/basicTool/RegistObjectWithPinyin.java
@@ -0,0 +1,91 @@
+package basicTool;
+
+import com.github.stuxuhai.jpinyin.PinyinException;
+import com.github.stuxuhai.jpinyin.PinyinFormat;
+import com.github.stuxuhai.jpinyin.PinyinHelper;
+
+import basicInterface.IPinyinHolder;
+
+public class RegistObjectWithPinyin extends RegistObject implements IPinyinHolder {
+	public String pinyin;
+	public String shortPinyin;
+	
+	public RegistObjectWithPinyin(String index, String name) {
+		super(index, name);
+		setPinyin(null);
+		setShortPinyin(null);
+	}
+	
+	/**
+	 * 获取名字的拼音
+	 * @return
+	 *  	名字的拼音。
+	 */
+	@Override
+	public String getPinyin() {
+		return pinyin;
+	}
+	
+	/**
+	 * 设置名字，
+	 * 这个方法在NameWithPinyin重载，
+	 * 就是为了保证拼音与名字任何时刻都要保持一致。
+	 * @param name
+	 * 		要设置的名字。
+	 */
+	@Override
+	public void setName(String name){
+		this.name = name;
+		setPinyin(null);
+		setShortPinyin(null);
+	}
+
+	/**
+	 * 本方法不会利用参数，
+	 * 而是直接从父类的NameHolder中获取名字然后将汉字转换为拼音。
+	 * @param pinyinName
+	 * 		本方法不会用参数做任何设置。
+	 */
+	@Override
+	public void setPinyin(String pinyinName) {
+		try {
+			this.pinyin = PinyinHelper.convertToPinyinString(getName(), "", PinyinFormat.WITHOUT_TONE);
+			return;
+		} catch (PinyinException e) {
+			MyLogger.logError("设置拼音名字失败。请检查名字：" + getName());
+			MyLogger.logException(e);
+		}
+		this.pinyin = "";
+	}
+
+	/**
+	 * 返回短拼音字符串，
+	 * 就是将汉字换成对应拼音的首字母。
+	 * @return
+	 * 		名字的短拼音字符串。
+	 */
+	@Override
+	public String getShortPinyin() {
+		return shortPinyin;
+	}
+
+	/**
+	 * 设置名字的短拼音字符串。
+	 * @param shortPinyin
+	 * 		这个参数不会用到，可以随便设置为null，
+	 * 		这个方法或调用getName()方法获取名字，
+	 * 		然后转换成短拼音形式。
+	 */
+	@Override
+	public void setShortPinyin(String shortPinyin) {
+		try {
+			this.shortPinyin = PinyinHelper.getShortPinyin(getName());
+			return;
+		} catch (PinyinException e) {
+			MyLogger.logError("设置拼音名字失败。请检查名字：" + getName());
+			MyLogger.logException(e);
+		}
+		this.shortPinyin = "";
+	}
+
+}
