7b2dc0667f79cabf46949b994545d58ba7721c24
181847 <753090084@qq.com>
2017-02-23 16:37:58 +0800

    为过滤器创建一个抽象类。
    这个抽象类的功能主要是判断info参数是否为null，以及container是否为null，然后调用dealWithContainer()处理contianer，这样就保证container一定不是空指针，让用户更专注处理container中的信息。

---
+++ src/unfinishedClass/AbstractFilter.java
@@ -0,0 +1,32 @@
+package unfinishedClass;
+
+import basicTool.MyLogger;
+import infoInterface.IInfo;
+import infoInterface.IInfoFilter;
+
+public abstract class AbstractFilter implements IInfoFilter {
+
+	public abstract boolean dealWithContainer(Object container);
+
+	/**
+	 * 读取info对象并调用dealWithContainer来处理信息，
+	 * @return 
+	 * 		如果info为null或者container为null的话，返回false。
+	 */
+	@Override
+	public boolean check(IInfo info) {
+		if (info == null){
+			MyLogger.logError("AbstractFilter准备提取当前信息体的信息，但是信息体本身为null，"
+					+ "不可读取空指针，提取信息失败。");
+			return false;
+		}
+		Object container = info.getContainer();
+		if (container == null){
+			MyLogger.logError("AbstractFilter准备提取当前信息体的信息，"
+					+ "但是读取出来的container为null，"
+					+ "从container获取信息失败。");
+			return false;
+		}
+		return dealWithContainer(container);
+	}
+}
