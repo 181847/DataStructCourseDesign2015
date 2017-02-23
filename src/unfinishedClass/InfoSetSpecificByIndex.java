0853d120be2ff46ac8593389e7616baddb1e3ac9
181847 <753090084@qq.com>
2017-02-23 16:43:13 +0800

    InfoSetSpecificByIndex的功能重新思考修改
    现在将这个类的功能缩小，他的主要功能就是提供一个能够保证集合内部一定没有相同序号的信息体，以及可以通过序号删除特定的信息体。
    
    内部的registArea存储的Info对象内部的container是插入父类中的循环链表的对象（DoubleLinkedInfo类型，就是说DoubleLinkedInfo作为Container），这样子就可以通过序号直接找到这个循环链表内部的节点，然后由于双重循环链表的特点，这个节点可以完全依靠自己内部的数据，将自己从链表中删除掉。
    从而实现了这个类的保证信息体的序号不相同，和从指定的序号将信息体删除。

--- src/unfinishedClass/InfoSetSpecificByIndex.java
+++ src/unfinishedClass/InfoSetSpecificByIndex.java
@@ -1,16 +1,33 @@
 package unfinishedClass;
 
 import basicTool.MyLogger;
+import info.DoubleLinkedInfo;
+import info.InfoWithContainer;
+import info.infoTool.AllTrueFilter;
 import info.infoTool.IndexGetter;
+import info.infoTool.NameGetter;
 import infoInterface.IInfo;
+import infoInterface.IInfoFilter;
+import infoInterface.IInfoGetter;
 import infoSet.DoubleLoopLinkedInfoSet;
 import infoSet.InfoSearchTree;
 
+/**
+ * 本类并不能依靠Index参数获取相关信息体，
+ * 本类只是利用Index的数据确保这个集合体内部的信息体的序号是唯一的。
+ * @author 75309
+ *
+ */
 public class InfoSetSpecificByIndex extends DoubleLoopLinkedInfoSet{
-	InfoSearchTree iSTForIndex;
+	/**
+	 * 这个搜索树中存储的Info对象的Container是
+	 * DoubleLinedInfo对象，也就是说对iSTForIndex的Info对象进行一些操作的时候不能直接利用普通的Getter/Traverser/Filter，
+	 * 而是要使用专门用DLInfo的对应的Getter/Traverser/Filter。
+	 */
+	InfoSearchTree registArea;
 	
 	public InfoSetSpecificByIndex(){
-		iSTForIndex = new InfoSearchTree('\0');
+		registArea = new InfoSearchTree('\0');
 	}
 	
 	/**
@@ -30,12 +47,14 @@ public class InfoSetSpecificByIndex extends DoubleLoopLinkedInfoSet{
 					+ "插入失败。");
 			return 0;
 		}
+		
 		Object container = newInfo.getContainer();
 		if (container == null){
 			MyLogger.logError("InfoSetSpecificByIndex插入的元素中没有信息，"
 					+ "插入失败。");
 			return -1;
 		}
+		
 		String index = new IndexGetter().pickMessage(newInfo);
 		if (index.isEmpty()){
 			MyLogger.logError("InfoSetSpecificByIndex插入的元素中的container不是IIndexHolder的子类，"
@@ -43,17 +62,87 @@ public class InfoSetSpecificByIndex extends DoubleLoopLinkedInfoSet{
 			return -2;
 		}
 		
-		DoubleLoopLinkedInfoSet searchResult = 
-				iSTForIndex.getSpecificInfoSet(index.toCharArray(), 0);
-		if (searchResult == null || searchResult.isEmpty()){
-			super.insertInfo(newInfo);
-			this.iSTForIndex.insertInfo(newInfo, new IndexGetter());
+		//准备搜素序号信息体
+		char[] indexArray = index.toCharArray();
+		
+		if (! haveIndex(indexArray)){
+			DoubleLinkedInfo newDLInfo = new DoubleLinkedInfo(newInfo.getContainer());
+			newDLInfo.next = head.next;
+			newDLInfo.prec = head;
+			head.next = newDLInfo;
+			newDLInfo.next.prec = newDLInfo;
+			++num;
+			
+			registArea.insertInfoAccordToCharArray(index.toCharArray(), 0, new InfoWithContainer(newDLInfo));
 			return 1;
+			
 		} else {
 			MyLogger.logError("InfoSetSpecificByIndex中已经包含同序号的信息，"
 					+ "插入失败。"
 					+ "插入元素的序号：" + index);
 			return -3;
 		}
+	}//insertInfo
+	
+	/**
+	 * 检查序号是否已经存在。
+	 * @param indexArray 
+	 * 		序号数组。
+	 * @return 
+	 * 		序号已存在返回真，
+	 * 		序号不存在返回假。
+	 */
+	public boolean haveIndex(char[] indexArray){
+		DoubleLoopLinkedInfoSet searchResult = 
+				registArea.getSoloInfoSet(indexArray, 0);
+		return ! (searchResult == null || searchResult.isEmpty());
+	}
+	
+	/**
+	 * 删除这个InfoSet中的指定要求的对象，
+	 * 将所有被删除的对象用一个DLLInfoSet返回。
+	 * @param searchInfo 
+	 * 		主要用来检查的字符串信息，
+	 * 		只有与这个searchInfo相等才算找到，
+	 * 		如果这个字符串为空串的话表示所有信息体都符合，
+	 * 		如果这些信息体同时通过了filter的筛选的话，才会删除。
+	 * @param getter 
+	 * 		这个对象用来获取Info信息体中指定的信息，
+	 * 		比如某个InfoGetter用来获取Info中的名字信息，
+	 * 		有的InfoGetter用来获取Info中的学号信息。
+	 * @param filter 
+	 * 		过滤器，用于针对Info中的信息返回真假，
+	 * 		最终的InfoSet会过滤掉所有filter检查结果为false的Info，
+	 * 		比如某个filter规定如果StudentInfo的年级为大一，
+	 * 		那么这次删除只会删除大一的StudentInfo。
+	 * @return 
+	 * 		删除的所有Info组成的InfoSet。
+	 */
+	@Override
+	public DoubleLoopLinkedInfoSet delete(String searchInfo, IInfoGetter getter, IInfoFilter filter){
+		DoubleLoopLinkedInfoSet deletedInfoSet = super.delete(searchInfo, getter, filter);
+		if ( ! deletedInfoSet.isEmpty() ){
+			registArea.delete(searchInfo, new InnerGetterForInfo(getter), new InnerFilterForInfo(filter));
+		}
+		return deletedInfoSet;
+	}
+	
+	/**
+	 * 传入一个指定的序号，
+	 * 将指定序号的信息体从对象内删除，
+	 * 将这个对象通过一个DLLInfoSet返回
+	 * @param index
+	 * @return
+	 */
+	public DoubleLoopLinkedInfoSet deleteIndex(String index){
+		char[] indexArray = index.toCharArray();
+		DoubleLoopLinkedInfoSet infoSet = registArea.getSoloInfoSet(indexArray, 0);
+		DeleteTraverserForDLInfo deleteTraverser = new DeleteTraverserForDLInfo();
+		
+		if ( ! (infoSet == null || infoSet.isEmpty()) ){
+			infoSet.traverseInfo(deleteTraverser, new AllTrueFilter());
+			registArea.soloDelete(indexArray, 0, "", new InnerGetterForInfo(new NameGetter()), new AllTrueFilter());
+		}
+		return deleteTraverser.getDLLInfoSet();
 	}
 }
