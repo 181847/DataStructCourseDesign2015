bedaf11cd10bea4371edbf6e079201c918759027
181847 <753090084@qq.com>
2017-02-23 17:03:57 +0800

    测试环境中添加一些基础的工具，方便测试用例调用

--- src/testSpace/Test.java
+++ src/testSpace/Test.java
@@ -1,9 +1,23 @@
 package testSpace;
 
+import info.infoTool.AllTrueFilter;
+import info.infoTool.GenderGetterForStudent;
+import info.infoTool.GradeGetterForStudent;
+import info.infoTool.IndexGetter;
+import info.infoTool.MainCourseGetterForStudent;
+import info.infoTool.NameGetter;
+import info.infoTool.StudentTraverser;
 import unfinishedClass.Student;
 
 public class Test {
 	public static Student[] stus;
+	public static StudentTraverser studentTraverser;
+	public static NameGetter nameGetter;
+	public static IndexGetter indexGetter;
+	public static GenderGetterForStudent genderGetter;
+	public static GradeGetterForStudent gradeGetter;
+	public static MainCourseGetterForStudent mainCourseGetter;
+	public static AllTrueFilter allTrueFilter;
 	
 	public static void prepare(){
 		stus = new Student[]{
@@ -18,5 +32,16 @@ public class Test {
 				new Student("2002009", "许强", 1, "大三", "机械理论"), 
 				new Student("20020010", "杨小鹃", 2, "大一", "文化与法制"), 
 				new Student("20020011", "岳不群", 1, "大一", "体育")};
+		prepareVariables();
+	}
+
+	public static void prepareVariables() {
+		studentTraverser = new StudentTraverser();
+		nameGetter = new NameGetter();
+		indexGetter = new IndexGetter();
+		genderGetter = new GenderGetterForStudent();
+		gradeGetter = new GradeGetterForStudent();
+		mainCourseGetter = new MainCourseGetterForStudent();
+		allTrueFilter = new AllTrueFilter();
 	}
 }
