35d3a953b32161014e9d6d290eb96e3ec707f10d
181847 <753090084@qq.com>
2017-02-24 00:09:47 +0800

    这个特殊的搜索目录父类是SearchLog，之添加了一个有关删除的方法
    这个搜索目录只能对应于包含序号的信息体来创建搜索目录，这个目录专门为了在搜索目录中删除Info的时候，删除指定的字符路径，而不删除不同序号的Info，比如说现在的搜索目录是一个学生名字的搜索目录，有两个人的名字一样，但是学号不同比如“2015
    张三”、“3578 张三”，这是有可能的，当在名字搜索目录中删除“2015 张三” 的时候不能只依靠父类的删除方法，父类的删除方法会将“3578
    张三”也一并删除，所以这个添加的这个方法直接传入一个info类型对象，从这个info对象中获取学号，和名字，然后在名字搜索目录中删除名字的时候只针对特定的学号，学号不同的不用管。
    为什么这里要穿入一个Info对象再来删除呢？
    
    因为搜索目录是附属于InfoSetSpecificByIndex的对象，只有InfoSetSpecificByIndex中删除对象的时候，才会删除相应搜索目录中的对象，然而InfoSetSpecificByIndex删除对象的时候要求必须一个序号一个序号的删除，所以这个时候我们往往不知到他的名字，但是却能从删除返回的集合中直接找到这个被删除出来的对象，与其在外部将这个被删除的对象的序号和名字提取出来，再放进名字搜索目录中进行删除操作，这个时候往往具有局限性，因为我们可能还要在拼音搜索目录中删除、在性别搜索目录中删除、在年级搜索目录中删除......，总不能每一个目录都专门写代码提取信息，然后传到目录中删除吧！这时候，直接利用各个目录中的成员变量Getter来获取信息，这样的话就要把Info对象传进去，既然Info对象传进去了，那么干脆连获取序号也在内部获取算了。

---
+++ src/unfinishedClass/SearchLogForIndex.java
@@ -0,0 +1,54 @@
+package unfinishedClass;
+
+import basicTool.MyLogger;
+import info.infoTool.AllTrueFilter;
+import info.infoTool.IndexGetter;
+import infoInterface.IInfo;
+import infoInterface.IInfoGetter;
+
+/**
+ * 这个搜索目录专门为信息体包含的container是IIndexHolder的子类，
+ * 添加一个删除方法可以区分Index的方法
+ * @author 75309
+ *
+ */
+public class SearchLogForIndex extends SearchLog {
+
+	/**
+	 * 创建目录时要用到信息体的那一部分信息作为字符路径。
+	 * @param keyGetter
+	 * 		获取作为字符路径的Getter。
+	 */
+	public SearchLogForIndex(IInfoGetter keyGetter) {
+		super(keyGetter);
+	}
+	
+	/**
+	 * 将在指定路径结尾的信息体全部删除。
+	 * @param searchInfo
+	 * 		字符串路径。
+	 * @return
+	 * 		参数字符串为null或者空串时返回0，
+	 * 		如果删除时发生任何错误也返回0，
+	 * 		删除成功返回1。
+	 */
+	public int deleteInfo(IInfo infoWithIndex){
+		if (infoWithIndex == null){
+			MyLogger.logError("SearchLogForIndex调用deleteInfo(IInfo)时发生错误，参数为null");
+			return 0;
+		}
+		String index = new IndexGetter().pickMessage(infoWithIndex);
+		String searchInfo = keyGetter.pickMessage(infoWithIndex);
+		if (searchInfo.isEmpty()){
+			MyLogger.logError("SearchLogForIndex调用deleteInfo(IInfo)时发生错误，"
+					+ "获取的字符路径为空。");
+			return -1;
+		}
+		if (soloDelete(searchInfo.toCharArray(), 0, 
+				index, new IndexGetter(), new AllTrueFilter()) 
+				== 1){
+			return 1;
+		}
+		return 0;
+	}
+}
