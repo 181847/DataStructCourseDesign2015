9fd29046527d79c3a9b0d1f21cae368d26a14406
181847 <753090084@qq.com>
2017-02-25 23:39:50 +0800

    为MyClub和MyMember创建Traverser

---
+++ src/unfinishedClass/MyClubTraverser.java
@@ -0,0 +1,25 @@
+package unfinishedClass;
+
+import basicTool.MyLogger;
+import info.infoTool.AbstractTraverser;
+
+public class MyClubTraverser extends AbstractTraverser {
+
+	@Override
+	public int dealWithContainer(Object container) {
+		if (! (container instanceof MyClub)){
+			MyLogger.log("错误：StudentTraverser读取info的container的类型不是Student，无法读取信息。");
+			return 0;
+		}
+		
+		MyClub tempContainer = (MyClub) container;
+		System.out.println("社团编号：" + tempContainer.getIndex() 
+				+ "\t社团名字：" + tempContainer.getName()
+				+ "\n社团名字拼音：" + tempContainer.getPinyin()
+				+ "\t社团名字短拼音：" + tempContainer.getShortPinyin()
+				+ "\n在社团中的职位：" + tempContainer.getPosition());
+		MyLogger.seperate();
+		return 1;
+	}
+
+}
