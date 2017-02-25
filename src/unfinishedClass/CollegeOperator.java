92e3c2e4cc244b301a92e29e75514115f5658847
181847 <753090084@qq.com>
2017-02-25 21:38:32 +0800

    创建CollegeOperator
    这个类拥有一个College对象，使得所有的操作都能很直接对College中的对象进行修改。

---
+++ src/unfinishedClass/CollegeOperator.java
@@ -0,0 +1,19 @@
+package unfinishedClass;
+
+import basicInterface.IOperator;
+
+public abstract class CollegeOperator implements IOperator {
+	public College college;
+	
+	/**
+	 * @param college
+	 * 		操作员的操作对象。
+	 */
+	public CollegeOperator(College college){
+		this.college = college;
+	}
+
+	@Override
+	public abstract int operate();
+
+}
