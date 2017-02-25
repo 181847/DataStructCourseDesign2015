b915a5f6ca405bfd34cff3151190c058042c6cfb
181847 <753090084@qq.com>
2017-02-25 21:50:39 +0800

    专门为college中的studentInfoSet和clubInfoSet添加的搜索操作对象，父类是SearchOperator
    用过这个操作对象，可以对college中的学生或者社团方便的定制想要搜索的对象，比如只在姓名目录中搜索某个字符串，或者只在性别搜索目录中搜索女性。

---
+++ src/unfinishedClass/SearchOperatorForClubs.java
@@ -0,0 +1,30 @@
+package unfinishedClass;
+
+public class SearchOperatorForClubs extends SearchOperator {
+
+	/**
+	 * 0 - 序号；
+	 * 1 - 名字；
+	 * 2 - 拼音名字；
+	 * 3 - 短拼音名字；
+	 * 4 - 社团创建时间。
+	 */
+	public SearchOperatorForClubs(College college) {
+		super(college);
+		selectedLog = new boolean[5];
+	}
+
+	/**
+	 * 执行搜索college中社团的操作，
+	 * 搜索的结果存在searchResult中，
+	 * 可以通过getResult()方法获取搜索结果。
+	 * @return
+	 * 		1。
+	 */
+	@Override
+	public int operate() {
+		searchResult = college.getClubInfoSet().search(searchInfo, selectedLog);
+		return 1;
+	}
+
+}
