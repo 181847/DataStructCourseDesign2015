0a324fc4904c84c67709de505c6710a5a94e5dd9
181847 <753090084@qq.com>
2017-02-26 15:04:34 +0800

    导入路径修改

--- src/testSpace/TestInfoSet.java
+++ src/testSpace/TestInfoSet.java
@@ -1,10 +1,10 @@
 package testSpace;
 
+import collegeComponent.Student;
+import collegeComponent.tool.StudentTraverser;
 import info.DoubleLinkedInfo;
 import info.infoTool.AllTrueFilter;
-import info.infoTool.StudentTraverser;
 import infoSet.DoubleLoopLinkedInfoSet;
-import unfinishedClass.Student;
 
 public class TestInfoSet extends Test{
 	public static void main(String[] args){
