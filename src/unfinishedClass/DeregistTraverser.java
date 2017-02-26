8e73ad23f59f7b1aef9f3ecb445f6731a4811880
181847 <753090084@qq.com>
2017-02-27 00:00:37 +0800

    为带有注销功能的遍历者创建一个父类抽象类
    这个抽象类中自带一个DeregisterOperator成员，由子类设定要注销的社团编号和社员学号。

---
+++ src/unfinishedClass/DeregistTraverser.java
@@ -0,0 +1,24 @@
+package unfinishedClass;
+
+import collegeComponent.College;
+import info.infoTool.AbstractTraverser;
+import operator.DeregisterOperator;
+
+public abstract class DeregistTraverser extends AbstractTraverser {
+	public DeregisterOperator dro;
+	
+	public DeregistTraverser(College college){
+		dro = new DeregisterOperator(college);
+	}
+	
+	@Override
+	public abstract int dealWithContainer(Object container);
+	
+	public void setStudentIndex(String studentIndex){
+		dro.setStudentIndex(studentIndex);
+	}
+	
+	public void setClubIndex(String clubIndex){
+		dro.setClubIndex(clubIndex);
+	}
+}
