0253c6ad78da9e0b0010cb92adedbc1e318bddf9
181847 <753090084@qq.com>
2017-02-26 14:30:01 +0800

    创建一个删除学生的操作器
    删除学生不仅要从college.studentInfoSet中删掉学生，还要注销掉他参加的所有社团。
    注销社团的时候专门用一个带有DeregisterOperator的Traverser遍历MyClubs，然后注销每个社团。

---
+++ src/unfinishedClass/StudentDeleteOperator.java
@@ -0,0 +1,44 @@
+package unfinishedClass;
+
+import basicTool.MyLogger;
+import info.infoTool.AllTrueFilter;
+import infoSet.InfoSetSpecificByIndex;
+
+public class StudentDeleteOperator extends DeleteOperator {
+
+	public StudentDeleteOperator(College college) {
+		super(college);
+	}
+
+	/**
+	 * @return
+	 * 		如果目标序号不符合规范，
+	 * 	就返回0；
+	 * 		如果不存在指定序号的学生，
+	 * 	返回-1；
+	 * 		成功返回1。
+	 */
+	@Override
+	public int operate() {
+		if (check()){
+			Student student = college.getStudent(targetIndex);
+			if (student == null){
+				MyLogger.logError("StudentDeleteOperator要删除的对象不存在，"
+						+ "请检查：" + targetIndex);
+				return -1;
+			}
+			InfoSetSpecificByIndex myClubs = student.getMyClubs();
+			
+			DeregistTraverserForMyClub dtfmc = 
+					new DeregistTraverserForMyClub(
+							getCollege(), getTargetIndex());
+			
+			myClubs.traverseInfo(dtfmc, new AllTrueFilter());
+			college.getStudentInfoSet().deleteIndex(targetIndex);
+			return 1;
+		}
+		return 0;
+	}
+	
+	
+}
