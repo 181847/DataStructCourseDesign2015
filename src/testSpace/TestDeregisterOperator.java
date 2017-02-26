a68744f5e8b1db12803c41af0501b2c9bd157bd5
181847 <753090084@qq.com>
2017-02-26 14:15:32 +0800

    添加DeregisterOperator类的测试用例

---
+++ src/testSpace/TestDeregisterOperator.java
@@ -0,0 +1,94 @@
+package testSpace;
+
+import basicTool.MyLogger;
+import unfinishedClass.DeregisterOperator;
+import unfinishedClass.RegisterOperator;
+
+public class TestDeregisterOperator extends Test {
+
+	public static void main(String[] args) {
+		prepare();
+		
+		RegisterOperator ro = new RegisterOperator(college);
+		ro.setPosition("会员");
+		ro.setStudentIndex("2002015");
+		ro.setClubIndex("2012003");
+		ro.operate();
+		
+
+		ro.setPosition("会员");
+		ro.setStudentIndex("2002009");
+		ro.setClubIndex("2012003");
+		ro.operate();
+
+		ro.setPosition("会长");
+		ro.setStudentIndex("2002005");
+		ro.setClubIndex("2012003");
+		ro.operate();
+
+		ro.setPosition("会长");
+		ro.setStudentIndex("20020011");
+		ro.setClubIndex("2012005");
+		ro.operate();
+		
+		college.getClub("2012003").getMyMembers().traverseInfo(myMemberTraverser, allTrueFilter);
+		
+		MyLogger.seperate("Another Club");
+		college.getClub("2012005").getMyMembers().traverseInfo(myMemberTraverser, allTrueFilter);
+		
+		MyLogger.seperate("Student Clubs\n\n\n");
+		MyLogger.seperate();
+		MyLogger.seperate();
+		college.getStudent("2002015").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("2002009").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("2002005").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("20020011").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		
+		DeregisterOperator dro = new DeregisterOperator(college);
+		
+		dro.setStudentIndex("2002015");
+		dro.setClubIndex("2012003");
+		dro.operate();
+		
+		dro.setStudentIndex("2002009");
+		dro.setClubIndex("2012003");
+		dro.operate();
+		
+		MyLogger.seperate();
+		MyLogger.seperate();
+		MyLogger.seperate(" After Delete ");
+		
+		college.getStudent("2002015").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("2002009").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("2002005").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("20020011").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		
+		dro.setStudentIndex("2002005");
+		dro.setClubIndex("2012003");
+		dro.operate();
+		
+		MyLogger.seperate();
+		MyLogger.seperate();
+		MyLogger.seperate(" After Delete ");
+		
+		college.getStudent("2002015").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("2002009").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("2002005").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		MyLogger.seperate("Another Student");
+		college.getStudent("20020011").getMyClubs().traverseInfo(myClubTraverser, allTrueFilter);
+		
+		MyLogger.seperate(" The Club \n\n");
+		college.getClub("2012003").getMyMembers().traverseInfo(myMemberTraverser, allTrueFilter);
+		MyLogger.seperate("Another Club");
+		college.getClub("2012005").getMyMembers().traverseInfo(myMemberTraverser, allTrueFilter);
+	}
+
+}
