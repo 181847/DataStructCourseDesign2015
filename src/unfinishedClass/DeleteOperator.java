e919f1012b5c102734fa031b6e446e998d2bf778
181847 <753090084@qq.com>
2017-02-26 14:27:06 +0800

    创建一个用来删除的Operator抽象类
    内部包含一个Index成员，指明要删除的对象的序号，子类既可以用这个成员来表示学生的学号，也可以用来表示社团的编号。

---
+++ src/unfinishedClass/DeleteOperator.java
@@ -0,0 +1,35 @@
+package unfinishedClass;
+
+import basicTool.MyLogger;
+
+public abstract class DeleteOperator extends CollegeOperator {
+	public String targetIndex;
+
+	public DeleteOperator(College college) {
+		super(college);
+	}
+
+	@Override
+	public abstract int operate();
+
+	public String getTargetIndex() {
+		return targetIndex;
+	}
+
+	public void setTargetIndex(String targetIndex) {
+		this.targetIndex = targetIndex;
+	}
+	
+	protected boolean check(){
+		if (targetIndex == null){
+			MyLogger.logError("DeleteOperator的删除对象的序号为null，"
+					+ "请设置要删除的对象的序号");
+			return false;
+		} else if (targetIndex.isEmpty()){
+			MyLogger.logError("DeleteOperator的删除对象的序号为空串，"
+					+ "请设置要删除的对象的序号");
+			return false;
+		}
+		return true;
+	}
+}
