4e419440ac7a95490cc6016f2624fb5f65023dd1
181847 <753090084@qq.com>
2017-02-27 23:56:54 +0800

    根据之前Demo类的尝试，将搜索需要的组件集合起来创建一个SearchPanel。
    通过这个Panel，可以很方便的添加一系列有关搜索的可视化组件，需要指定用于搜索的Operator，Table中用来显示的Column，与SearchOperator中指定搜索类型相匹配的ModelTraverser，ModelTraverser中的Column由这个类的内部自动设定为参数中的column，请注意保持ModelTraverser添加到Model中的信息要与column也要匹配。

---
+++ src/unfinishedClass/SearchPanel.java
@@ -0,0 +1,111 @@
+package unfinishedClass;
+
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.DefaultTableModel;
+
+import basicTool.MyLogger;
+import info.infoTool.AllTrueFilter;
+import operator.SearchOperator;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class SearchPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6342938097939446042L;
+	private JTextField searchTextField;
+	private JTable resultTable;
+	private String[] column;
+	private SearchOperator so;
+	private ModelTraverser traverser;
+	
+	public SearchPanel(SearchOperator so, String[] column, ModelTraverser traverser) {
+		this.so = so;
+		this.column = column;
+		this.traverser = traverser;
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel controlPanel = new JPanel();
+		add(controlPanel, BorderLayout.NORTH);
+		
+		searchTextField = new JTextField();
+		controlPanel.add(searchTextField);
+		searchTextField.setColumns(10);
+		
+		JButton btnSearch = new JButton("Search");
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				showSearchResult();
+			}
+		});
+		controlPanel.add(btnSearch);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+		
+		resultTable = new JTable(){
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isCellEditable(int rowIndex, int columnIndex){
+				return false;
+			}
+		};
+		scrollPane.setViewportView(resultTable);
+		
+		searchTextField.getDocument().addDocumentListener(new DocumentListener(){
+ 
+            public void changedUpdate(DocumentEvent e) {
+            	showSearchResult();
+            }
+ 
+            public void insertUpdate(DocumentEvent e) {
+                //差入时响应
+            	showSearchResult();
+            }
+ 
+            public void removeUpdate(DocumentEvent e) {
+                //删除时响应
+            	showSearchResult();
+            }});
+		
+		
+		
+		
+		//showSearchResult();
+	}
+
+	private void showSearchResult() {
+		String searchInfo = searchTextField.getText();
+		MyLogger.log("SearchInfo: " + (searchInfo == null));
+		traverser.setModel(new DefaultTableModel(column, 0));
+		so.setSearchInfo(searchInfo);
+		so.selectSearchLog(-1);
+		so.operate();
+		so
+			.getResult()
+			.getAllResult(new AllTrueFilter())
+			.traverseInfo(traverser, new AllTrueFilter());
+		
+		resultTable.setModel(traverser.getModel());
+	}
+	
+	public JTable getResultTable(){
+		return resultTable;
+	}
+
+}
