d74a98e974b3c6c42b76b9e866cf25660b982053
181847 <753090084@qq.com>
2017-02-28 23:09:41 +0800

    先前用于测试bug用到的测试用例。

--- src/testSpace/TestSearchOperatorForMyMembers.java
+++ src/testSpace/TestSearchOperatorForMyMembers.java
@@ -1,16 +1,50 @@
 package testSpace;
 
+import basicTool.MyLogger;
+import operator.ClubPositionOperator;
+import operator.DeregisterOperator;
+import operator.StudentUpdateOperator;
 import unfinishedClass.SearchOperatorForMyMembers;
 
 public class TestSearchOperatorForMyMembers extends Test {
 	public static void main(String[] args){
 		prepare();
-		Test.showCollege();
-		SearchOperatorForMyMembers sofmm = new SearchOperatorForMyMembers(college, "2012003");
+		//Test.showCollege();
+		SearchOperatorForMyMembers sofmm = new SearchOperatorForMyMembers(college, "2012005");
 		sofmm.setSearchInfo("");
 		sofmm.selectSearchLog(-1);
 		sofmm.operate();
 		sofmm.getResult().getAllResult(allTrueFilter).traverseInfo(myMemberTraverser, allTrueFilter);
+		
+		/**
+		ClubPositionOperator cpo = new ClubPositionOperator(college);
+		cpo.setClubIndex("2012005");
+		cpo.setStudentIndex("2002009");
+		cpo.setPosition("秘书");
+		cpo.operate();
+		
+		sofmm.operate();
+		sofmm.getResult().getAllResult(allTrueFilter).traverseInfo(myMemberTraverser, allTrueFilter);
+		*/
+		
+		StudentUpdateOperator suo = new StudentUpdateOperator(college);
+		suo.setStudent(college.getStudent("2002009"));
+		suo.setNewIndex("200200978");
+		suo.operate();
+		
+		MyLogger.seperate("After Change Index");
+		sofmm.operate();
+		sofmm.getResult().getAllResult(allTrueFilter).traverseInfo(myMemberTraverser, allTrueFilter);
+		
+		DeregisterOperator dro = new DeregisterOperator(college);
+		dro.setClubIndex("2012005");
+		dro.setStudentIndex("200200978");
+		dro.operate();
+		
+		MyLogger.seperate("After Dregist");
+		sofmm.operate();
+		sofmm.getResult().getAllResult(allTrueFilter).traverseInfo(myMemberTraverser, allTrueFilter);
+		
 	}
 
 }
