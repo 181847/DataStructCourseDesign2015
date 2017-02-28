2ca6b340afd26590a776907b91da4aa15c476fee
181847 <753090084@qq.com>
2017-02-28 23:08:51 +0800

    为Student类专门添加的一种MyClubModelTraverser
    用来遍历内部的所有参加的社团，获取有关社团的编号、名字和在社团中的职位信息。

---
+++ src/unfinishedClass/MyClubModelTraverser.java
@@ -0,0 +1,38 @@
+package unfinishedClass;
+
+import javax.swing.table.DefaultTableModel;
+
+import basicTool.MyLogger;
+import collegeComponent.MyClub;
+
+public class MyClubModelTraverser extends ModelTraverser {
+
+	public MyClubModelTraverser(DefaultTableModel model) {
+		super(model);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MyClubModelTraverser(Object[] column){
+		super(column);
+	}
+	
+	public MyClubModelTraverser(){
+		super();
+	}
+
+	@Override
+	public int dealWithContainer(Object arg0) {
+		if (arg0 instanceof MyClub){
+			MyClub myClub = (MyClub) arg0;
+			String index = myClub.getIndex();
+			String name = myClub.getName();
+			String position = myClub.getPosition();
+			model.addRow(new Object[]{index, name, position});
+			return 1;
+		} else {
+			MyLogger.logError("ClubModelTraverser遍历的container不是MyMember类型"
+					+ "无法获取社团信息。");
+			return 0;
+		}
+	}
+}
