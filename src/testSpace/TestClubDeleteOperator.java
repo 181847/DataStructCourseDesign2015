fd74105f6b9c115fa3a23bf9797b499147607a19
181847 <753090084@qq.com>
2017-03-01 14:40:02 +0800

    修改先前测试ClubDeleteOperator的测试用例。
    删除Club之前先对club的信息进行修改，然后删除，最后发现，当修改了Index的信息时，由于更新Club信息的时候并没有对Student内部进行修改，导致Student.myClubsInfoSet中任然保留着原来Club的序号索引，没有将序号跟新。

--- src/testSpace/TestClubDeleteOperator.java
+++ src/testSpace/TestClubDeleteOperator.java
@@ -2,6 +2,7 @@ package testSpace;
 
 import basicTool.MyLogger;
 import operator.ClubDeleteOperator;
+import operator.ClubUpdateOperator;
 
 public class TestClubDeleteOperator extends Test {
 
@@ -10,14 +11,21 @@ public class TestClubDeleteOperator extends Test {
 		college.getClubInfoSet().traverseInfo(clubTraverser, allTrueFilter);
 		college.getStudentInfoSet().traverseInfo(studentTraverser, allTrueFilter);
 		
-		ClubDeleteOperator cdo = new ClubDeleteOperator(college, "2012003");
-		cdo.operate();
+		
 		
 		MyLogger.newLine();
 		MyLogger.newLine();
 		MyLogger.newLine();
 		MyLogger.newLine();
 		MyLogger.newLine();
+		
+		ClubUpdateOperator cuo = new ClubUpdateOperator(college, college.getClub("2012003"));
+		
+		cuo.setNewIndex("201200345");
+		cuo.operate();
+		
+		ClubDeleteOperator cdo = new ClubDeleteOperator(college, "201200345");
+		cdo.operate();
 		
 		college.getClubInfoSet().traverseInfo(clubTraverser, allTrueFilter);
 		college.getStudentInfoSet().traverseInfo(studentTraverser, allTrueFilter);
