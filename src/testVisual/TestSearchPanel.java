426ebf5e2b2f834450045591982ea369f8b3ed1e
181847 <753090084@qq.com>
2017-03-02 17:25:04 +0800

    根据SearchPanel的新定义，修改TestSearchPanel的测试用例。

--- src/testVisual/TestSearchPanel.java
+++ src/testVisual/TestSearchPanel.java
@@ -3,11 +3,14 @@ package testVisual;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
 
 import aboutVisual.SearchPanel;
 import collegeComponent.tool.traverser.ClubModelTraverser;
 import operator.SearchOperatorForClubs;
 import testSpace.Test;
+import unfinishedClass.AbstractTableNotifier;
+import unfinishedClass.ClubFrame;
 
 public class TestSearchPanel extends JFrame {
 	public SearchPanel searchPanel;
@@ -17,7 +20,20 @@ public class TestSearchPanel extends JFrame {
 										new String[]{
 											"社团编号", "社团名字", "创建日期"
 														},
-										new ClubModelTraverser());
+										new ClubModelTraverser(),
+										new AbstractTableNotifier() {
+											@Override
+											public boolean fire(int rowIndex, int columnIndex, JTable table){
+													if (columnIndex == 0){
+														JFrame clubFrame = new ClubFrame(Test.college, 
+																(String) table.getValueAt(rowIndex, columnIndex));
+														
+														clubFrame.setLocationRelativeTo(table);
+														clubFrame.setVisible(true);
+													}//if
+													return false;
+												}//fire()
+											});//AbstractTableNotifier
 		searchPanel.getResultTable().setEnabled(true);
 		this.getContentPane().add(searchPanel);
 	}
