e3657105ad6990905a26ca6426d245464779fe0b
181847 <753090084@qq.com>
2017-03-02 17:33:34 +0800

    无关紧要的格式修改

--- src/testVisual/TestClubFrame.java
+++ src/testVisual/TestClubFrame.java
@@ -9,10 +9,10 @@ import unfinishedClass.CollegeFrame;
 public class TestClubFrame extends Test{
 	public static void main(String[] args){
 		prepare();
-		
+
 		JFrame frame = new ClubFrame(college, "2012003");
 		frame.setVisible(true);
-		
+
 		JFrame frame1 = new CollegeFrame(college);
 		frame1.setVisible(true);
 	}
